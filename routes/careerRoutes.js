cdbd9bd2d4080f62b712bf25677304899d096799
Sarbesh Kumar Tiwari <89567003+sarbeshtiwari@users.noreply.github.com>
2024-08-28 13:07:43 +0530

    updated

--- routes/careerRoutes.js
+++ routes/careerRoutes.js
@@ -4,19 +4,7 @@ const careerController = require('../controllers/careerController');
 const upload = require('../middlewares/careerQuery_multerMiddlewares');
 
 // Routes
-router.post('/addCareer', upload.single('resume'), (req, res) => {
-    try {
-        res.send('File uploaded successfully.');
-    } catch (error) {
-        if (error instanceof multer.MulterError) {
-            // A Multer error occurred when uploading.
-            res.status(500).send(error.message);
-        } else {
-            // An unknown error occurred.
-            res.status(500).send('An error occurred during file upload.');
-        }
-    }
-}, careerController.createCareer);
+router.post('/addCareer', upload.single('resume'),  careerController.createCareer);
 router.get('/getCareer', careerController.getCareer);
 router.delete('/deleteCareer/:id', careerController.deleteCareer);
 router.put('/updateCareer/:id', careerController.updateCareer);
