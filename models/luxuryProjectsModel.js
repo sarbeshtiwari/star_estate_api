c2419d4bc471fe9061cdbb1ab7bd3d22196d489d
Sarbesh Kumar Tiwari <89567003+sarbeshtiwari@users.noreply.github.com>
2024-08-28 14:37:30 +0530

    "Rename 'projectName' field to 'project_name' in QuerySchema"

--- models/luxuryProjectsModel.js
+++ models/luxuryProjectsModel.js
@@ -4,7 +4,7 @@ const QuerySchema = new mongoose.Schema({
     Name: { type: String, required: true },
     Email: { type: String, required: true },
     phoneNumber: { type: Number, required: true },
-    projectName: {type: String, required : true},
+    project_name: {type: String, required : true},
     user_query: { type: String, required: true },
     created_at: { type: Date, required: true },
     note: { type: String, required: false },
