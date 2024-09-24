b8301207298a275055f06c73ace4a02910b53ec7
Sarbesh Kumar Tiwari <89567003+sarbeshtiwari@users.noreply.github.com>
2024-09-24 18:37:06 +0530

    updated

--- middlewares/projectGallery_multerMiddleware.js
+++ middlewares/projectGallery_multerMiddleware.js
@@ -16,38 +16,14 @@ const storage = new CloudinaryStorage({
     params: {
         folder: 'star_estate/project_gallery',
         allowed_formats: ['jpg', 'png', 'jpeg', 'webp', 'svg'],
-        public_id: (req, file) => sanitizeFilename(file.originalname.split('.')[0]), // Sanitize filename without extension
+        public_id: (req, file) => {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+            return sanitizeFilename(file.originalname.split('.')[0]) + '_' + uniqueSuffix; // Append unique suffix to filename
+        },
     },
 });
 
-  
-const upload = multer({ storage: storage });  
-
-// // Set up storage for multer
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         const uploadDir = 'uploads/project_gallery';
-//         if (!fs.existsSync(uploadDir)) {
-//             fs.mkdirSync(uploadDir, { recursive: true });
-//         }
-//         cb(null, uploadDir);
-//     },
-//     filename: function (req, file, cb) {
-//         const ext = path.extname(file.originalname);
-//         const filename = Date.now() + ext;
-//         cb(null, filename);
-//     }
-// });
-
-// const upload = multer({ 
-//     storage: storage, 
-//     fileFilter: (req, file, cb) => {
-//         const ext = path.extname(file.originalname);
-//         if (['.jpg', '.jpeg', '.png', '.gif'].includes(ext.toLowerCase())) {
-//             return cb(null, true);
-//         }
-//         cb(new Error('Only image files are allowed.'));
-//     }
-// });
+const upload = multer({ storage: storage });
 
 module.exports = upload;
+
